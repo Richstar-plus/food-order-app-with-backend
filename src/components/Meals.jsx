0864788ec132ad678c5e5e4359e468d3c7cd42d5
richard <richardsunday0812@gmail.com>
2026-03-09 12:01:24 +0100

    Add Header and Meals components to the application

---
+++ src/components/Meals.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+      // Handle the fetched data
+
+      if (!response.ok) {
+        // Handle the error
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
